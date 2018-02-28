a61e6f88ddbab10158fbc227e0da2b27cc703485
Joana Ramos <joanasmramos98@gmail.com>
2018-02-28 23:28:35 +0000

    Thought of a better strategy, started implementing

--- TP1eclipse/src/dkeep/logic/Character.java
+++ TP1eclipse/src/dkeep/logic/Character.java
@@ -176,61 +176,47 @@ class Ogre extends Character {
 
 class Guard extends Character{
 
-   protected char guardTraject[] = {'A','S','S','S','S','A','A','A','A','A', 'A', 'S','D','D','D','D','D','D','D',
+   private char guardTraject[] = {'A','S','S','S','S','A','A','A','A','A', 'A', 'S','D','D','D','D','D','D','D',
            'W','W','W','W','W'};
-   protected int trajInd = 0;
+   private int trajInd = 0;
+   private boolean asleep;
+   private boolean reverse;
+   private Random rand = new Random();
+   private char reverseTraject[] = {'s', 'd', 'w', 'w', 'w', 'w', 'd', 'd', 'd', 'd', 'd', 'd', 'w', 'a', 'a','a', 'a', 'a', 'a', 'a', 's', 's', 's', 's', 's'};
 
 
-   public Guard(int line, int column, char charc) {super(line, column, charc);}
+   public Guard(int line, int column, char charc) {
+	   super(line, column, charc);
+	   asleep = false;
+	   reverse = false;
+   }
 
    public int getInd(){return trajInd;}
 
-   public void incInd(){if(trajInd==guardTraject.length-1){
+   public void incInd(){
+	   if(trajInd==guardTraject.length-1){
        trajInd = 0;
-   }else trajInd++;;}
+	   }
+	   else trajInd++;
+   }
 
+   public void decInd() {
+	   if(trajInd == 0) {
+		   trajInd = guardTraject.length - 1;
+	   }
+	   else trajInd--;
+   }
+   
     public char getDir(int i){return guardTraject[i];}
-
-    public void moveChar(){
-
-       switch (guardTraject[trajInd]){
-           case 'W':
-               this.line--;
-               return;
-           case 'S':
-               this.line++;
-               return;
-           case 'D':
-               this.column++;
-               return;
-           case 'A':
-               this.column--;
-
-           default:
-       }
-    }
-}
-
-class DrunkenGuard extends Guard{
-	private boolean asleep;
-	private boolean reverse;
-	private Random rand = new Random();
-	private char reverseTraject[] = {'s', 'd', 'w', 'w', 'w', 'w', 'd', 'd', 'd', 'd', 'd', 'd', 'w', 'a', 'a','a', 'a', 'a', 'a', 'a', 's', 's', 's', 's', 's'};
-	
-	public DrunkenGuard(int line, int column, char charc) {
-		super(line, column, charc);
-		asleep = false;
-		reverse = false;
-	}
-	
-	public boolean generateBool() {
+    
+    public boolean generateBool() {
 		int trigger = rand.nextInt(1);
 		if(trigger == 0)
 			return true;
 		else return false;
-	}
-	
-	public void moveChar(){
+    }
+
+    public void moveChar(){
 		asleep = generateBool();
 		reverse = generateBool();
 		
@@ -271,7 +257,7 @@ class DrunkenGuard extends Guard{
 				}
 				trajInd++;
 			}
-		}
+    }
 }
-	
+
 	
