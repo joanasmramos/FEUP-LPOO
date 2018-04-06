231ba5c05288827e0b439266c06a8b4b79e7f53d
Joana Ramos <joanasmramos98@gmail.com>
2018-04-06 16:02:59 +0100

    Some more

--- src/dkeep/logic/Object.java
+++ src/dkeep/logic/Object.java
@@ -6,10 +6,20 @@ public class Object implements GameElement{
     protected char charo;
     protected boolean visible;
 
+    /**
+     * Constructs a visible object
+     */
     public Object(){
     	this.visible=true;
     	}
 
+    /**
+     * Constructs an object in the given position, represented internally by the given char
+     * Sets visible to true
+     * @param line: object's line
+     * @param column: object's column
+     * @param charo: object's representative char
+     */
     public Object(int line, int column, char charo){
         this.line = line;
         this.column = column;
@@ -17,6 +27,9 @@ public class Object implements GameElement{
         this.visible = true;
     }
 
+    /**
+     * @return true if object is visible, false otherwise
+     */
     public boolean isVisible() {
         return visible;
     }
@@ -34,16 +47,27 @@ public class Object implements GameElement{
         this.column = column;
     }
 
+    /**
+     * @return returns object's representative char
+     */
     public char getChar(){
     	if(visible)
     		return charo;
     	else return ' ';
     }
 
+    /**
+     * Sets the object's representative char
+     * @param c: new char
+     */
     public void setChar(char c){
         this.charo = c;
     }
     
+    /**
+     * Sets visible
+     * @param visible: true if object's visible, false otherwise
+     */
     public void setVisible(boolean visible) {
     	this.visible = visible;
     }
--- src/dkeep/logic/Ogre.java
+++ src/dkeep/logic/Ogre.java
@@ -5,6 +5,10 @@ import dkeep.logic.*;
 
 import dkeep.logic.GameState.States;
 
+/**
+ * @author Joana
+ *
+ */
 public class Ogre extends Character {
     private boolean stunned = false;
     private int turnsLeft = 2;
@@ -12,40 +16,73 @@ public class Ogre extends Character {
     private char dir;
     private Club ogre_club = new Club(super.line,super.column,'*');
     
+    /**
+     * Constructs an ogre in the given position, represented by the given char
+     * Sets stunned to false (ogre's not stunned)
+     * Sets turnsLeft to 2 (if the ogre gets stunned, he will remain stunned for two turns)
+     * @param line: ogre's line
+     * @param column: ogre's column
+     * @param charc: ogre's representative char
+     */
     public Ogre(int line, int column, char charc) {
         super(line, column, charc);
         stunned = false;
         turnsLeft = 2;
     }
 
+    /**
+     * Constructs an ogre represented by the given char
+     * Sets stunned to false (ogre's not stunned)
+     * Sets turnsLeft to 2 (if the ogre gets stunned, he will remain stunned for two turns)
+     * @param charc: ogre's representative char
+     */
     public Ogre(char charc) {
 		super(1, 3, charc);
 		stunned = false;
 		turnsLeft = 2;
 	}
 
+	/**
+	 * Sets stunned
+	 * @param stun: true if stunned, false otherwise
+	 */
 	public void setStunned(boolean stun) {
         stunned = stun;
     }
 
+    /**
+     * @return returns flag stunned
+     */
     public boolean getStunned() {
         return stunned;
     }
 
 
+    /**
+     * Generates an integer in the given interval
+     * @param n1: lower bound
+     * @param n2: upper bound
+     * @return
+     */
     public static int generateNr(int n1, int n2) {
         return rand.nextInt(n2 - n1 + 1) + n1;
     }
 
 
+    /**
+     * Sets number of turns left
+     * @param turns: turns left
+     */
     public void setTurns(int turns) {
         this.turnsLeft = turns;
     }
 
+    /**
+     * @return returns number of turns left
+     */
     public int getTurns() {
         return turnsLeft;
     }
-
 
     public char getChar() {
         if(stunned)
@@ -54,14 +91,24 @@ public class Ogre extends Character {
     }
 
 
+    /**
+     * @return returns direction of the ogre's movement
+     */
     public char getDir() {
         return dir;
     }
 
+    /**
+     * @return returns ogre's club
+     */
     public Club getOgre_club() {
         return ogre_club;
     }
 
+    /**
+     * Throws ogre's club in a given direction and updates its coordinates
+     * @param dir: direction for the club's movement
+     */
     public void throwClub(char dir) {
         this.getOgre_club().setVisible(true);
 
@@ -80,6 +127,10 @@ public class Ogre extends Character {
         }
     }
 
+    /**
+     * Generates a direction
+     * @return returns generated direction
+     */
     public char generateDir() {
         switch(rand.nextInt(4)){
             case 0:
@@ -94,10 +145,18 @@ public class Ogre extends Character {
         }
     }
 
+    /**
+     * Sets ogre's direction of movement
+     * @param dir: new direction
+     */
     public void setOgreDir(char dir){
         this.dir = dir;
     }
 
+    /**
+     * Updates ogre's coordinates according to its movement, updates club
+     * @param dir: direction of the movement
+     */
     public void moveChar(char dir) {
     	updateCoords(dir);
 
@@ -105,6 +164,10 @@ public class Ogre extends Character {
     }
 
 
+    /**
+     * Updates turns left according to the stunned flag
+     * @return
+     */
     public int manageTurns(){
 
             if(getTurns() != 0) {
