526dac20709d1052993f50649a0bd45441a9eb1b
Joana Ramos <joanasmramos98@gmail.com>
2018-04-06 16:16:04 +0100

    Finished documentation

--- src/dkeep/logic/Rookie.java
+++ src/dkeep/logic/Rookie.java
@@ -2,6 +2,12 @@ package dkeep.logic;
 
 public class Rookie extends Guard{
 
+    /**
+     * Constructs a Rookie Guard in the given position, represented internally by the given char
+     * @param line: guard's line
+     * @param column: guard's column
+     * @param charc: guard's representative char
+     */
     public Rookie(int line, int column, char charc) {
         super(line, column, charc);
     }
--- src/dkeep/logic/Suspicious.java
+++ src/dkeep/logic/Suspicious.java
@@ -2,6 +2,12 @@ package dkeep.logic;
 
 public class Suspicious extends Guard {
 
+    /**
+     * Constructs a new Suspicious Guard in the given position, represented internally by the given char
+     * @param line: guard's line
+     * @param column: guard's column
+     * @param charc: guard's representative char
+     */
     public Suspicious(int line, int column, char charc) {
         super(line, column, charc);
     }
