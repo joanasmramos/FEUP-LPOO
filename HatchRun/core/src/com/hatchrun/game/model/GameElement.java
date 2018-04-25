763a069c797a0fdb71a93c9a22340fbbe7ef4ea3
Joana Ramos <joanasmramos98@gmail.com>
2018-04-25 17:55:05 +0100

    Started models

---
+++ HatchRun/core/src/com/hatchrun/game/model/GameElement.java
@@ -0,0 +1,34 @@
+package com.hatchrun.game.model;
+
+/**
+ * A model representing a game element which has a certain lane
+ */
+public class GameElement {
+    public enum ElementLane {LEFT, CENTER, RIGHT};
+
+    private ElementLane lane;
+
+    /**
+     * Constructs a game element
+     * @param lane The current lane of the element
+     */
+    GameElement(ElementLane lane) {
+        this.lane = lane;
+    }
+
+    /**
+     * Returns the element's lane
+     * @return Element's lane
+     */
+    public ElementLane getLane() {
+        return lane;
+    }
+
+    /**
+     * Sets the element's lane to the given lane
+     * @param lane Given lane
+     */
+    public void setLane(ElementLane lane) {
+        this.lane = lane;
+    }
+}
---
+++ HatchRun/core/src/com/hatchrun/game/model/Hatch.java
@@ -0,0 +1,102 @@
+package com.hatchrun.game.model;
+
+/**
+ * A model representing a hatch (the only game element user-controlled)
+ */
+
+public class Hatch extends GameElement {
+    private enum HatchType {BLUE, GREEN, PURPLE, YELLOW};
+
+    private int coins;
+    private int score;
+    private boolean shielded;
+    private HatchType currentHatch;
+
+    /**
+     * Constructs a hatch (default = purple) with no coins, zero score, not shielded
+     * @param lane The current lane of the hatch
+     */
+    Hatch(ElementLane lane) {
+        super(lane);
+        coins = 0;
+        score = 0;
+        shielded = false;
+        currentHatch = HatchType.PURPLE;
+    }
+
+    /**
+     * Constructs the default hatch: purple, with no coins, zero score, not shielded
+     */
+    Hatch() {
+        super(ElementLane.CENTER);
+        coins = 0;
+        score = 0;
+        shielded = false;
+        currentHatch = HatchType.PURPLE;
+    }
+
+    /**
+     * Returns hatch's coins
+     * @return Hatch's coins
+     */
+    public int getCoins() {
+        return coins;
+    }
+
+    /**
+     * Sets hatch's coins
+     * @param coins Hatch's coins
+     */
+    public void setCoins(int coins) {
+        this.coins = coins;
+    }
+
+    /**
+     * Returns hatch's score
+     * @return Score
+     */
+    public int getScore() {
+        return score;
+    }
+
+    /**
+     * Sets hatch's score
+     * @param score Score
+     */
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    /**
+     * Returns true if the hatch is shielded, false otherwise
+     * @return shielded
+     */
+    public boolean isShielded() {
+        return shielded;
+    }
+
+    /**
+     * Shields/unshields the hatch
+     * @param shielded True if shielded, false otherwise
+     */
+    public void setShielded(boolean shielded) {
+        this.shielded = shielded;
+    }
+
+    /**
+     * Returns the hatch's colour
+     * @return hatch's colour
+     */
+    public HatchType getCurrentHatch() {
+        return currentHatch;
+    }
+
+    /**
+     * Sets the hatch's colour
+     * @param currentHatch Colour
+     */
+    public void setCurrentHatch(HatchType currentHatch) {
+        this.currentHatch = currentHatch;
+    }
+}
+
