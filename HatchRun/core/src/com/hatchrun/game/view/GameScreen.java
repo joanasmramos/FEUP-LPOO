1637a0be214ad138b15953f0f02e8f1316fccbf1
Beatriz <bea.smendes98@gmail.com>
2018-04-25 16:09:01 +0100

    Strated estructuring project.

---
+++ HatchRun/core/src/com/hatchrun/game/view/GameScreen.java
@@ -0,0 +1,13 @@
+package com.hatchrun.game.view;
+
+import com.badlogic.gdx.ScreenAdapter;
+import com.hatchrun.game.HatchRun;
+
+public class GameScreen extends ScreenAdapter{
+     HatchRun game;
+
+    public GameScreen(HatchRun game){
+        this.game = game;
+    }
+
+}
