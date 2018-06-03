9a574fa423f1675d0100973139b67a16a49cd970
Joana Ramos <joanasmramos98@gmail.com>
2018-06-03 14:58:06 +0100

    game over menu, not definitive

--- HatchRun/core/src/com/hatchrun/game/view/GameOverView.java
+++ HatchRun/core/src/com/hatchrun/game/view/GameOverView.java
@@ -2,6 +2,7 @@ package com.hatchrun.game.view;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -13,6 +14,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.hatchrun.game.HatchRun;
 import com.hatchrun.game.model.GameModel;
@@ -23,18 +25,14 @@ public class GameOverView extends ScreenAdapter {
     private Label scoreLabel;
     private Label youLost;
     private Stage stage;
-    private BitmapFont bmap;
     private HatchRun game;
     private Table table;
     private Table table2;
-    private TextButton.TextButtonStyle style;
     private ScreenViewport viewport;
     private Image background;
 
 
     public GameOverView(HatchRun game) {
-        createStyle();
-
         this.game = game;
         viewport = new ScreenViewport();
         stage = new Stage(viewport);
@@ -42,7 +40,8 @@ public class GameOverView extends ScreenAdapter {
         setUpExitButton();
         setUpLabels();
         setUpTable();
-        setUpTable2();
+        //setUpTable2();
+        Gdx.input.setInputProcessor(stage);
     }
 
     private void setUpTable2() {
@@ -55,35 +54,44 @@ public class GameOverView extends ScreenAdapter {
         table2.setFillParent(true);
     }
 
-    private void createStyle() {
-        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/knewave.ttf"));
+    private BitmapFont createBmapFont(int size, String file) {
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal(file));
         FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
-        parameter.size = 150;
-        bmap = generator.generateFont(parameter);
-        style = new TextButton.TextButtonStyle();
+        parameter.size = size;
+        BitmapFont bmap = generator.generateFont(parameter);
+        return bmap;
+    }
+
+    private TextButton.TextButtonStyle createStyle(BitmapFont bmap) {
+        TextButton.TextButtonStyle style = new TextButton.TextButtonStyle();
         style.font = bmap;
+
+        return style;
     }
 
     private void setUpTable() {
         table = new Table();
 
+        table.bottom();
+        table.setFillParent(true);
         table.add(youLost);
         table.row();
         table.add(scoreLabel);
         table.row();
         table.add(exitButton);
+        table.padBottom(40);
 
         stage.addActor(table);
 
     }
 
     private void setUpLabels() {
-        youLost = new Label("You lost! Your score: ", new Label.LabelStyle(bmap, Color.PINK));
-        scoreLabel = new Label(String.format("%06d", GameModel.getInstance().getScore()), new Label.LabelStyle(bmap, Color.PINK));
+        youLost = new Label("You lost! Your score: ", new Label.LabelStyle(createBmapFont(70, "fonts/knewave-outline.ttf"), Color.PINK));
+        scoreLabel = new Label(String.format("%06d", GameModel.getInstance().getScore()), new Label.LabelStyle(createBmapFont(100, "fonts/knewave.ttf"), Color.PINK));
     }
 
     public void setUpExitButton() {
-        exitButton = new TextButton("Exit", style);
+        exitButton = new TextButton("Exit", createStyle(createBmapFont(150, "fonts/knewave-outline.ttf")));
 
         exitButton.addListener(new ClickListener(){
             @Override
@@ -95,6 +103,10 @@ public class GameOverView extends ScreenAdapter {
 
     @Override
     public void render(float delta) {
+        game.getBatch().begin();
+        Texture t = game.getAssetManager().get("mainmenu.png", Texture.class);
+        game.getBatch().draw(t, 0, 0,Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        game.getBatch().end();
         stage.act();
         stage.draw();
     }
